eaca81b0212f776d8970f0596541c9198e47d0d8
bhargavi <bhargaviborgohain@gmail.com>
2024-10-05 06:00:43 +0530

    models and db creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,29 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from .views import views
 from .auth import auth
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ajkbdasbf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_NAME
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+    create_db(app)
+
     return app
+
+def create_db(app):
+    if not os.path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created database!', DB_NAME)
